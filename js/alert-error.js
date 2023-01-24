4c2559c4f1463fd21a37c19824817563bdc41b72
Amanda <113949375+amandalventura@users.noreply.github.com>
2023-01-24 13:59:58 -0300

    Add files via upload

---
+++ js/alert-error.js
@@ -0,0 +1,8 @@
+export const AlertError = {
+  element: document.querySelector('.alert-error'),
+  open() {
+    AlertError.element.classList.add('open')},
+    close() {
+      AlertError.element.classList.remove('open')
+    }
+}
